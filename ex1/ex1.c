311650bbc7b2a50dffc4995b0e0a96dc95e38f04
20101 <20101@LAPTOP-I6CLUL77>
2022-09-29 12:35:14 +0200

    first commit

---
+++ ex1/ex1.c
@@ -0,0 +1,15 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Sep 28, 2022
+ *      Author: 20101
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf(" c programming ");
+	return EXIT_SUCCESS;
+}
+
+
